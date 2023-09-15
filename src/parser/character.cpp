50c61d747d55005e4ac76b96a857233928a5a7ca
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-09-16 01:36:10 +0900

    Impl character parser

--- src/parser/character.cpp
+++ src/parser/character.cpp
@@ -3,9 +3,12 @@
 
 Parser<char> auto character(char c)
 {
-    return [=](std::string_view input)
+    return [=](std::string_view input) -> std::optional<ParserResult<char>>
     {
-        // TODO
+        if (input.empty())
+            return std::nullopt;
+        if (input.front() == c)
+            return ParserResult<char>{c, input.substr(1)};
         return std::nullopt;
     };
 }
