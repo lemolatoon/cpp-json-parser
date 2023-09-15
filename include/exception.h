08cae613ad95da2a9851a4de80843597d2fc1aff
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-09-16 01:30:50 +0900

    Add exception dir

---
+++ include/exception.h
@@ -0,0 +1,16 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+#include <string>
+
+class ParserException : public std::exception
+{
+private:
+    const std::string message_;
+    const std::string_view input_rest_;
+
+public:
+    ParserException(std::string message, std::string_view input_rest) : message_(message), input_rest_(input_rest){};
+    const char *what() const noexcept override;
+};
+
+#endif // EXCEPTION_H
---
+++ src/exception/exception.cpp
@@ -0,0 +1,7 @@
+#include <exception.h>
+#include <string>
+
+const char *ParserException::what() const noexcept
+{
+    return message_.data();
+}
