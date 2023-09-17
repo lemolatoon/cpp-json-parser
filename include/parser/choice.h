b88ad999d3ca474c27033ffd65c9d18b6dbcabbd
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-09-17 22:56:42 +0900

    Impl parsers::choices

--- include/parser/choice.h
+++ include/parser/choice.h
@@ -3,6 +3,7 @@
 #include "parser.h"
 #include <concepts>
 #include <optional>
+#include <tuple>
 #include <type_traits>
 
 namespace parsers {
@@ -26,6 +27,35 @@ auto inline choice(Parser auto primary_parser, Parser auto secondary_parser)
     }
 
     return secondary_parser(input);
+  };
+}
+
+template <typename T, typename... Ts>
+concept are_same = std::conjunction_v<std::is_same<T, Ts>...>;
+
+auto inline choices(Parser auto primary_parser, Parser auto... parsers_rest)
+    -> Parser<ParserReturnType<decltype(primary_parser)>> auto{
+  // assert all parsers' ReturnType are same
+  static_assert(are_same<ParserReturnType<decltype(primary_parser)>,
+                         ParserReturnType<decltype(parsers_rest)>...>);
+
+  return choices_inner(primary_parser, parsers_rest...);
+}
+
+static auto inline choices_inner(Parser auto primary_parser,
+                                 Parser auto... parsers_rest) {
+  return [=](std::string_view input)
+             -> std::optional<
+                 ParserResult<ParserReturnType<decltype(primary_parser)>>> {
+    auto result = primary_parser(input);
+    if (result.has_value())
+      return result;
+
+    if constexpr (sizeof...(parsers_rest) > 0) {
+      return choices_inner(parsers_rest...)(input);
+    } else {
+      return std::nullopt;
+    }
   };
 }
 
--- test/choice_test.cpp
+++ test/choice_test.cpp
@@ -1,5 +1,8 @@
+#include "parser/character.h"
+#include "parser/skip_whitespace.h"
 #include "parsers.h"
 #include "gtest/gtest.h"
+#include <random>
 #include <vector>
 
 TEST(ParserTest, Choice) {
@@ -17,4 +20,52 @@ TEST(ParserTest, Choice) {
 
   auto parsed3 = parser("fuga * 123");
   EXPECT_TRUE(!parsed3.has_value());
+}
+
+TEST(ParserTest, Choices) {
+  using namespace parsers;
+  enum Kind {
+    Digit,
+    Null,
+    A,
+    B,
+    Open,
+    Close,
+  };
+  auto digitp = map(digits, [](auto d) { return Kind::Digit; });
+  auto nullp = map(string("null"), [](auto s) { return Kind::Null; });
+  auto ap = map(character('a'), [](auto c) { return Kind::A; });
+  auto bp = map(character('b'), [](auto c) { return Kind::B; });
+  auto openp = map(choices(character('{'), character('('), character('[')),
+                   [](auto c) { return Kind::Open; });
+  auto closep = map(choices(character('}'), character(')'), character(']')),
+                    [](auto c) { return Kind::Close; });
+
+  auto parser =
+      many(skip_whitespace(choices(digitp, nullp, ap, bp, openp, closep)));
+
+  auto parsed1 = parser("11 (456)null ").value();
+  EXPECT_EQ(parsed1.value, (std::vector{Kind::Digit, Kind::Open, Kind::Digit,
+                                        Kind::Close, Kind::Null}));
+  EXPECT_EQ(parsed1.remaining, " ");
+
+  auto parsed2 = parser("null {*} 123").value();
+  EXPECT_EQ(parsed2.value, (std::vector{Kind::Null, Kind::Open}));
+  EXPECT_EQ(parsed2.remaining, "*} 123");
+
+  auto parsed3 = parser("11 ([{]})Null ").value();
+  EXPECT_EQ(parsed3.value, (std::vector{
+                               Kind::Digit,
+                               Kind::Open,
+                               Kind::Open,
+                               Kind::Open,
+                               Kind::Close,
+                               Kind::Close,
+                               Kind::Close,
+                           }));
+  EXPECT_EQ(parsed3.remaining, "Null ");
+
+  auto parsed4 = parser(" fuga * 123").value();
+  EXPECT_EQ(parsed4.value, (std::vector<Kind>{}));
+  EXPECT_EQ(parsed4.remaining, " fuga * 123");
 }
